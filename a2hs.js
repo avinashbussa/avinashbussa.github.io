70d7e90957924cf264639d0cfe4f6de9df7c96e6
Avinash Bussa <abhi.patel345@gmail.com>
2021-05-12 16:18:27 +0530

    A2HS snippet added
    Updated files:
     1. a2hs.js
     2. index.html
     3. style.css
     4. Removed: Apple_Watch_Series_6
    (vector).svg

---
+++ a2hs.js
@@ -0,0 +1,59 @@
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  setTimeout(function(){ 
+    showInstallPromotion();
+  }, 4000);
+  setTimeout(function(){ 
+    hideInstallPromotion();
+  }, 11000);
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired. Now it's your turn!`);
+});
+
+installBtn.addEventListener('click', async () => {
+    // Hide the app provided install promotion
+    hideInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(`User response to the install prompt: ${outcome}`);
+    // We've used the prompt, and can't use it again, throw it away
+    deferredPrompt = null;
+});
+
+closeBtn.addEventListener('click', async () => {
+        // Hide the app provided install promotion
+        hideInstallPromotion();
+        console.log(`It's not fair, we thought you loved our app`);
+});
+
+window.addEventListener('appinstalled', () => {
+    // Hide the app-provided install promotion
+    hideInstallPromotion();
+    // Clear the deferredPrompt so it can be garbage collected
+    deferredPrompt = null;
+    // Optionally, send analytics event to indicate successful install
+    console.log('Yay! thanks for installing our app. We love you!');
+});
+
+function showInstallPromotion(){
+    var snackbar = document.getElementById("snackbar");
+    snackbar.style.transform="translateX(0%)";
+}
+  
+function hideInstallPromotion(){
+    var snackbar = document.getElementById("snackbar");
+    snackbar.style.transform="translateX(120%)";
+    setTimeout(function(){ 
+        snackbar.style.display="none";
+      }, 1000);
+}
